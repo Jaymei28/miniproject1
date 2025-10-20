59daa2609c10d07a0ce4244ffc00ef24514ca402
jaymei <jaymeirey.cortez@msunaawan.edu.ph>
2025-10-20 19:20:48 +0800

    CBV

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 from api.views import student_function_base_view
 from api.views import employee_function_base_view
+from api.views import student_class_base_view
+from api.views import employee_class_base_view
 
 
 urlpatterns = [
-    path('fbv_students/', student_function_base_view.StudentsList),
-    path('fbv_student/<int:student_id>/', student_function_base_view.student),
+   path('fbv_students/', student_function_base_view.StudentsList),
+    path('fbv_student/<int:student_id>/', student_function_base_view.student), 
 
-    path('fbv_employees/', employee_function_base_view.EmployeesList),
-    path('fbv_employee/<int:employee_id>/', employee_function_base_view.employee)
+    path('cbv_students/', student_class_base_view.Students.as_view()),
+    path('cbv_student_detail/<int:pk>/', student_class_base_view.StudentDetail.as_view()),
+
+    path('cbv_employees/', employee_class_base_view.Employees.as_view()),
+    path('cbv_employee_detail/<int:pk>/', employee_class_base_view.EmployeeDetail.as_view())
+
 ]
